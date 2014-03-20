92e6d24b7856900e7af75f2d0e99c4db69cf5a2f
Andrei Karpushonak <andrei@avrora.io>
2014-03-21 00:29:24 +0100

    Implemented verison 0.1.0

--- api_password.js
+++ api_password.js
@@ -1,6 +1,23 @@
 ApiPassword = {
+  validateUserPassword: function (username, password) {
 
+    var user = Meteor.users.findOne({username: username});
 
+    if (!user) {
+      throw new Error('User ' + username + ' not found');
+    }
 
+    if (!user.services || !user.services.password || !user.services.password.srp) {
+      throw new Error('User has no password set');
+    }
 
-}
+    var verifier = user.services.password.srp;
+    var newVerifier = SRP.generateVerifier(password, {identity: verifier.identity, salt: verifier.salt});
+
+    if (verifier.verifier !== newVerifier.verifier) {
+      throw new Error('Password is incorrect');
+    }
+
+    return 0;
+  }
+};
--- package.js
+++ package.js
@@ -1,19 +1,12 @@
 Package.describe({
-  summary: "Server-side password validation useful for REST API"
-});
-
-Package.on_test(function(api){
-  api.use("api-password", "server");
-  api.use(["tinytest", "test-helpers"], "server");
-
-  api.add_files("api_password_tests.js", "server");
+  summary: 'Server-side password validation useful for REST API with Meteor 0.7.2'
 });
 
 Package.on_use(function (api, where) {
-  api.use(["srp"], "server");
-
-  api.add_files(["api_password.js"], "server");
-
-  if (api.export)
-    api.export("ApiPassword");
+  api.use('srp', 'server');
+  api.add_files(['api_password.js'], 'server')
+  api.export('ApiPassword', 'server');
 });
+
+
+
